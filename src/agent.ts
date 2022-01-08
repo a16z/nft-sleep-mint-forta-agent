75659a1e078789c109acb5fe50d07871667bb10d
“blauyourmind” <blauyourmind@github.com>
2022-01-08 15:53:19 -0800

    add second strategy for approvals

--- src/agent.ts
+++ src/agent.ts
@@ -9,14 +9,16 @@ import {
   getEthersProvider
 } from 'forta-agent'
 
-import { BigNumber } from 'bignumber.js'
+import {
+  ZERO_ADDRESS,
+  ERC721_INTERFACE_ID,
+  TRANSFER_EVENT,
+  APPROVE_EVENT,
+  APPROVEAL_FOR_ALL_EVENT
+} from './constants'
+
 import { ethers } from 'ethers'
 import ERC721_ABI from "./ERC721_ABI.json";
-
-const ERC721_INTERFACE_ID = 0x5b5e139f
-const transferEvent = "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)";
-const approveEvent = ""
-const mintEvent = ""
 
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
   const findings: Finding[] = []
@@ -36,34 +38,61 @@ const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) =
     return findings
   }
 
-  const fromAddress = txEvent.from
-  const transfers = txEvent.filterLog(transferEvent, contract.address);
+  // get all transfer and approve events 
+  const txnSender = txEvent.from
 
+  const transfers = txEvent.filterLog(TRANSFER_EVENT, contract.address);
+
+  let approvals = txEvent.filterLog(APPROVE_EVENT, contract.address);
+  const approvalForAlls = txEvent.filterLog(APPROVEAL_FOR_ALL_EVENT, contract.address)
+  approvals.concat(approvalForAlls)
+
+  // check transfer events for mismatched txn sender and transferFrom address
   for (let transfer of transfers){
     const transferFromAddress = transfer.args.from
+    // ignore mint transactions which by default have mismatched transferFrom and txn sender addresses
+    if (transferFromAddress != txnSender && transferFromAddress != ZERO_ADDRESS){
 
-    if (transferFromAddress != fromAddress){
       findings.push(Finding.fromObject({
         name: "Sleeping Minted an NFT",
-        description: `An NFT Transfer was initiated by ${fromAddress} to transfer an NFT owned by ${transferFromAddress}`,
+        description: `An NFT Transfer was initiated by ${txnSender} to transfer an NFT owned by ${transferFromAddress}`,
         alertId: "SLEEPMINT-1",
         severity: FindingSeverity.Unknown,
         type: FindingType.Suspicious
       }))
+
     }
   }
+
+   // Method #2 if an NFT was minted and approved for re-transfer at the same time 
+
+  for (let approve of approvals){
+    const currentNFTOwner = approve.args.owner
+    const approvedAddress= approve.args.approved
+    
+    if (currentNFTOwner != txnSender && approvedAddress == txnSender){
+
+      findings.push(Finding.fromObject({
+        name: "Sleeping Minted an NFT",
+        description: `An NFT was approved for ${txnSender} but owned by ${currentNFTOwner}.`,
+        alertId: "SLEEPMINT-2",
+        severity: FindingSeverity.Medium,
+        type: FindingType.Suspicious
+      })) 
+
+    }
+
+  }
+
+
+
+  // Method #3 look at previous transactions for analysis 
 
   return findings
 }
 
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
 
 export default {
-  handleTransaction,
-  // handleBlock
+  handleTransaction
 }
